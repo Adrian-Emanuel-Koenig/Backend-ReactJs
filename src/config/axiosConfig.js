9cc879b8c8a32437621551956507a292fa869d8f
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2023-05-02 16:59:26 -0300

    arreglos

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,5 +2,7 @@ import Axios from "axios";
 
 Axios.defaults.withCredentials = true
 export const apiConnection = Axios.create({
-    baseURL:"backend-server-production-c1b9.up.railway.app"
+    baseURL:"https://backend-server-production-c1b9.up.railway.app"
+    // baseURL:"http://localhost:8080"
+
 })
